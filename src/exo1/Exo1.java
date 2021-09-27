16d058369f9542d56313d2163186562d8c481180
thvel <thvel@LAPTOP-43TL9KUU>
2021-09-27 12:18:51 +0200

    creation du depot

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,41 @@
+
+
+
+// Brun_Verceil TDC TP0 27/09/21
+
+
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author thvel
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        // TODO code application logic here
+        
+        String prenom;
+        Scanner sc;
+        
+        sc = new Scanner(System.in);
+             
+        System.out.println("Bonjour, quel est votre prenom? ");
+        prenom = sc.nextLine();
+        
+        System.out.println("Au revoir " + prenom);
+        
+        
+                
+          
+    }
+    
+    
+
+}
